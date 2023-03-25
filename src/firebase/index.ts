70f9ceb85fd296cde301890e5b494cde734dbbe8
Kádymo Santana <santanakadymo@gmail.com>
2023-03-25 02:48:09 +0000

    fix: Fixing environment variables path

--- src/firebase/index.ts
+++ src/firebase/index.ts
@@ -9,13 +9,13 @@ import { ConversationRef, UserRef } from "../types";
 import { currentUser } from "../main";
 
 export const firebaseApp = initializeApp({
-  apiKey: process.env.API_KEY,
-  authDomain: process.env.AUTH_DOMAIN,
-  projectId: process.env.PROJECT_ID,
-  storageBucket: process.env.STORAGE_BUCKET,
-  messagingSenderId: process.env.MESSAGING_SENDER_ID,
-  appId: process.env.APP_ID,
-  measurementId: process.env.MEASUREMENT_ID,
+  apiKey: import.meta.env.VITE_API_KEY,
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_APP_ID,
+  measurementId: import.meta.env.VITE_MEASUREMENT_ID,
 });
 
 const db = getFirestore(firebaseApp);
