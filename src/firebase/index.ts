0526b46a176f4003f043e110f4283fe546a4d61a
Kádymo Santana <santanakadymo@gmail.com>
2023-03-25 02:29:01 +0000

    Using environment variables

--- src/firebase/index.ts
+++ src/firebase/index.ts
@@ -9,13 +9,13 @@ import { ConversationRef, UserRef } from "../types";
 import { currentUser } from "../main";
 
 export const firebaseApp = initializeApp({
-  apiKey: "AIzaSyBbq3st8FTsi4RTr7kyb_BnflN1K9CQOKg",
-  authDomain: "chat-app-3ec25.firebaseapp.com",
-  projectId: "chat-app-3ec25",
-  storageBucket: "chat-app-3ec25.appspot.com",
-  messagingSenderId: "117312411887",
-  appId: "1:117312411887:web:30f9b67eb54112a063a31a",
-  measurementId: "G-8MJ0FMJ4M5",
+  apiKey: process.env.API_KEY,
+  authDomain: process.env.AUTH_DOMAIN,
+  projectId: process.env.PROJECT_ID,
+  storageBucket: process.env.STORAGE_BUCKET,
+  messagingSenderId: process.env.MESSAGING_SENDER_ID,
+  appId: process.env.APP_ID,
+  measurementId: process.env.MEASUREMENT_ID,
 });
 
 const db = getFirestore(firebaseApp);
